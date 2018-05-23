8c4c5978174233088e28c61698cf8609c8774a95
lentrix <gijhaycosmod123@gmail.com>
2018-05-23 11:41:55 +0800

    Updated

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=ws_carmina',
+    'username' => 'ws_carmina',
+    'password' => '18694RJG',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
